01e6b2cba59e3cf386d4921d03f2ff078bb8522b
Vinicius Ferreira Santos <vinifscuts@gmail.comgit config --global user.email vinifscuts@gmail.com>
2026-02-11 21:09:49 +0100

    Commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  base: "/Random-quote/",
+  base: "./",
+  plugins: [react()],
 });
